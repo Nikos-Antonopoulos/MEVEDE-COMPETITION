8ce3fdb3a8d0a82aa7d9e213646e7d8c9d815af5
antonopoulos <56845262+Nikos-Antonopoulos@users.noreply.github.com>
2020-11-28 13:56:01 +0200

    Create nodes.py

---
+++ nodes.py
@@ -0,0 +1,33 @@
+class Node:
+    def __init__(self, id, tp, dem, xx, yy):
+        self.id = id
+        self.type = tp
+        self.demand = dem
+        self.x = xx
+        self.y = yy
+all_nodes = []
+service_locations = []
+depot = Node(0, 0, 0, 50, 50)
+all_nodes.append(depot)
+random.seed(1)
+for i in range(0, 200):
+    id = i + 1
+    tp = random.randint(1,3)
+    dem = random.randint(1,5) * 100
+    xx = random.randint(0, 100)
+    yy = random.randint(0, 100)
+    serv_node = Node(id, tp, dem, xx, yy)
+    all_nodes.append(serv_node)
+    service_locations.append(serv_node)
+
+
+
+dist_matrix = [[0.0 for j in range(0, len(all_nodes))] for k in range(0, len(all_nodes))]
+for i in range(0, len(all_nodes)):
+    for j in range(0, len(all_nodes)):
+        source = all_nodes[i]
+        target = all_nodes[j]
+        dx_2 = (source.x - target.x)**2
+        dy_2 = (source.y - target.y) ** 2
+        dist = round(math.sqrt(dx_2 + dy_2))
+        dist_matrix[i][j] = dist 
