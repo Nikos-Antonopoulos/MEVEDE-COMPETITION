4441bfacd2725b7620e8dc82d932120ee6325192
GDMarkou <56844195+GDMarkou@users.noreply.github.com>
2021-01-06 14:16:51 +0200

    Update VRP_Model.py
    Implemented the time_matrix technic we talked about. !Important the matrix is still not symmetric because, the time when you are moving from the depot to a node and from a node to a depot is different.

--- VRP_Model.py
+++ VRP_Model.py
@@ -1,6 +1,6 @@
 import random
 import math
-
+import pprint
 
 class Model:
 
@@ -14,7 +14,7 @@ class Model:
         depot = Node(0, 0, 0, 50, 50)
         self.all_nodes.append(depot)
         random.seed(1)
-        number_of_service_points = 200
+        number_of_service_points = 5
 
         for i in range(0, number_of_service_points):  # Creation of the service locations
             id = i + 1
@@ -26,30 +26,49 @@ class Model:
             self.all_nodes.append(serv_node)
             self.service_locations.append(serv_node)
 
-        dist_matrix = [[0.0 for j in range(0, len(self.all_nodes))] for k in range(0, len(self.all_nodes))]  
-        for i in range(0, len(self.all_nodes)):  # Creation of the distance matrix
+        self.time_matrix = [[0.0 for j in range(0, len(self.all_nodes))] for k in range(0, len(self.all_nodes))]
+        for i in range(0, len(self.all_nodes)):  # Creation of the time matrix /George Markou
             for j in range(0, len(self.all_nodes)):
                 source = self.all_nodes[i]
                 target = self.all_nodes[j]
                 dx_2 = (source.x - target.x) ** 2
                 dy_2 = (source.y - target.y) ** 2
                 dist = round(math.sqrt(dx_2 + dy_2))
-                dist_matrix[i][j] = dist
-
-        for i in range(len(dist_matrix)):  # Creation of the time_matrix
-            self.time_matrix.append([])
-            for j in range(len(dist_matrix[i])):
-                if i == j:
-                    self.time_matrix[i].append(0.0)
-                elif self.all_nodes[j].type == 1:
-                    self.time_matrix[i].append(dist_matrix[i][j] / 35 * 60 + 5)
-                elif self.all_nodes[j].type == 2:
-                    self.time_matrix[i].append(dist_matrix[i][j] / 35 * 60 + 15)
-                elif self.all_nodes[j].type == 3:
-                    self.time_matrix[i].append(dist_matrix[i][j] / 35 * 60 + 25)
+                bonus_time = 0
+                if i == j:  # The distance Between the node and itself is 0
+                    self.time_matrix[i][j] = 0
                 else:
-                    self.time_matrix[i].append(dist_matrix[i][j] / 35 * 60)
-
+                    # In this section we get the sum of bonus_time from the target and source node
+                    if target.type == 1 and source.type == 1:  # Type 1 = 5 so (5+5)/2=5
+                        bonus_time = 5
+                    elif (target.type == 1 and source.type == 2) or (target.type == 2 and source.type == 1):
+                        # Type 1 = 5  and Type 2 = 15 so (15+5)/2=10
+                        bonus_time = 10
+                    elif target.type == 2 and source.type == 2:
+                        bonus_time = 15
+                    elif (target.type == 2 and source.type == 3) or (target.type == 3 and source.type == 2):
+                        bonus_time = 20
+                    elif target.type == 3 and source.type == 3:
+                        bonus_time = 25
+                    else:
+                        # if i==0 or j==0 which means its either going to the depot or moving away from it
+                        if target.type == 1:
+                            bonus_time = 5/2
+                        elif target.type == 2:
+                            bonus_time = 15/2
+                        elif target.type == 3:
+                            bonus_time = 25/2
+                        elif source.type == 1:
+                            bonus_time = 5/2
+                        elif source.type == 2:
+                            bonus_time = 15/2
+                        elif source.type == 3:
+                            bonus_time = 25/2
+                    time = dist / 35 * 60 + bonus_time
+                    if j == 0:  # If a route is returning to the depot the only extra cost is the bonus_time cost
+                        self.time_matrix[i][j] = bonus_time
+                    else:
+                        self.time_matrix[i][j] = time
 
 class Node:
     def __init__(self, id, tp, dem, xx, yy):
@@ -63,6 +82,7 @@ class Node:
 class Route:
     def __init__(self, dp, cap):  # One route, a node that's the depot, and the capacity
         self.sequenceOfNodes = []
+        self.sequenceOfNodes.append(dp)
         self.sequenceOfNodes.append(dp)
         self.cost = 0
         self.capacity = cap
