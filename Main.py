7fa276dd3349c2609cf8854ee527cf9d2390142a
giorgossideris <56915448+giorgossideris@users.noreply.github.com>
2021-01-07 17:52:35 +0200

    added a method in solver to check zachariadis' solution

--- Main.py
+++ Main.py
@@ -3,8 +3,9 @@ from VRP_Model import *
 import pprint
 m = Model()
 m.BuildModel()
-routes=m.load_objects()
-print(m.CalculateMaxCostOfRoute(routes,m.time_matrix))
+# routes=m.load_objects()
+# print(m.CalculateMaxCostOfRoute(routes,m.time_matrix))
 
 s = Solver(m)
+s.check_zach_example()
 sol = s.solve()
--- Solver.py
+++ Solver.py
@@ -88,6 +88,42 @@ class Solver:
         self.bestSolution = None
         self.searchTrajectory = []
 
+    def check_zach_example(self):
+        zach_routes = routes = [[0, 1, 50, 65, 101, 130, 150, 165, 183, 0],
+                  [0, 2, 46, 74, 77, 96, 120, 146, 179, 0],
+                  [0, 3, 34, 72, 104, 123, 145, 162, 194, 0],
+                  [0, 4, 40, 80, 103, 138, 163, 175, 198, 0],
+                  [0, 5, 49, 59, 70, 98, 106, 140, 166, 19, 0],
+                  [0, 6, 36, 62, 91, 107, 131, 178, 0],
+                  [0, 7, 33, 73, 99, 105, 126, 153, 169, 0],
+                  [0, 8, 47, 78, 115, 143, 167, 193, 0],
+                  [0, 9, 39, 61, 90, 100, 108, 119, 133, 159, 177, 187, 0],
+                  [0, 10, 51, 75, 85, 102, 127, 154, 173, 0],
+                  [0, 11, 48, 82, 111, 142, 156, 184, 0],
+                  [0, 12, 30, 41, 69, 93, 125, 151, 164, 0],
+                  [0, 13, 38, 79, 94, 141, 158, 196, 199, 0],
+                  [0, 14, 29, 60, 88, 117, 135, 155, 180, 200, 0],
+                  [0, 15, 43, 57, 95, 124, 148, 181, 189, 0],
+                  [0, 16, 52, 87, 122, 160, 192, 0],
+                  [0, 17, 27, 44, 76, 112, 137, 171, 188, 0],
+                  [0, 18, 31, 56, 64, 86, 116, 149, 174, 185, 0],
+                  [0, 19, 28, 55, 71, 110, 129, 168, 0],
+                  [0, 20, 32, 58, 84, 114, 147, 170, 191, 0],
+                  [0, 21, 37, 53, 67, 97, 144, 161, 197, 0],
+                  [0, 22, 35, 81, 118, 136, 157, 190, 0],
+                  [0, 23, 42, 68, 89, 128, 139, 176, 186, 0],
+                  [0, 24, 54, 92, 113, 134, 152, 182, 0],
+                  [0, 25, 26, 45, 63, 66, 83, 109, 121, 132, 172, 0]]
+        self.sol = Solution()
+        for zach_route in zach_routes:
+            new_route = Route(self.allNodes[0], 3000)
+            for zach_node in zach_route:
+                new_route.sequenceOfNodes.append(self.allNodes[zach_node])
+            self.sol.routes.append(new_route)
+        if (self.CalculateMaxCostOfRoute() == 16.271428571428572):
+            print("All good in zach's example")
+        else:
+            print("Something is wrong in zach's example")
     def solve(self):
         self.SetRoutedFlagToFalseForAllCustomers()
      #   self.ApplyNearestNeighborMethod()
