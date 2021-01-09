43161954946e6718d515b19d1a7eb96002aa0d33
giorgossideris <56915448+giorgossideris@users.noreply.github.com>
2021-01-09 14:14:10 +0200

    added random choice to VRPMinimumInsertions when two solutions of different customers have the same subjective cost

--- VRPMinimumInsertions.py
+++ VRPMinimumInsertions.py
@@ -1,5 +1,6 @@
 from VRP_Model import *
 from SolutionDrawer import *
+import random
 
 class Solution:
     def __init__(self): #sider
@@ -16,7 +17,9 @@ class CustomerInsertionAllPositions(object):
                                        # min max cost of the solution too
 
 class SolverMinIns:
-    def __init__(self, m, siders_constant):
+    def __init__(self, m, siders_constant, siders_constant2 = 0):
+        # m is the model, siders_constant, siders_constant2 (the second is optional) are numbers that define how
+        # important is it for an insertion not to increase the min_max cost of the solution
         self.allNodes = m.all_nodes
         self.customers = m.service_locations
         self.depot = m.all_nodes[0]
@@ -30,13 +33,15 @@ class SolverMinIns:
                                                # an insertion causes on the min max solution relatively to the
                                                # trialCost of the insertion (the cost on the route)
                                                # see find_best_insertion_for_customer for the use
+        self.siders_constant2 = siders_constant2
 
-    def solve(self, with_sort = False): # with sort variable defines if the minimum_insertions_with_opened_routes will
+    def solve(self, seed= 1 , with_sort = False): # with sort variable defines if the minimum_insertions_with_opened_routes will
                                         # sort the self.customers
+        random.Random(seed)
         self.SetRoutedFlagToFalseForAllCustomers()
         self.minimum_insertions_with_opened_routes(with_sort)
         # self.ReportSolution(self.sol)
-        #SolDrawer.draw(0, self.sol, self.allNodes)
+        SolDrawer.draw(seed, self.sol, self.allNodes)
         return self.sol
 
     def ReportSolution(self, sol):
@@ -112,6 +117,13 @@ class SolverMinIns:
             self.sol.routes.append(rt)  # the new route gets added to the solution
 
     def identify_best_insertion_in_all_routes(self, best_insertion):
+        insertions_with_same_subj_cost = 2 # used for reservoir sampling, it is the number of the insertions with the
+                                           # same subjective cost at the time of the choice and
+                                           # 1/insertions_with_the_same_subj_cost is the probability that a
+                                           # best_insertion_for_customer with the same subjective cost as
+                                           # the current best_insertion will replace it.
+                                           # It is initialized to 2 because the first time the sampling is used, there
+                                           # are 2 insertions with the same subjective cost
         for i in range(len(self.customers)): # iterate all customers
             candidate_cust: Node = self.customers[i] # the current checking customer
             if candidate_cust.isRouted is False: # if the customer is not routed
@@ -119,11 +131,22 @@ class SolverMinIns:
                 self.find_best_insertion_for_customer(candidate_cust, best_insertion_for_customer) # finds the best
                                                                                                    # insertion for the
                                                                                                    # particular customer
-                if best_insertion_for_customer.subjective_cost < best_insertion.subjective_cost:
+                if best_insertion_for_customer.subjective_cost < best_insertion.subjective_cost or \
+                        (best_insertion.subjective_cost == best_insertion_for_customer.subjective_cost #same subj_cost
+                         and random.random() > 1/insertions_with_same_subj_cost # random choice using reservoir sampling
+                        and len(best_insertion_for_customer.route.sequenceOfNodes) > 2): # don't take into considerations
+                                                                                      # the cases where routes contain
+                                                                                      # nothing more than depot
                     # if the best insertion of the checking customer is better than the best insertion at this moment
                     # according to the subjective cost (the cost that takes into account how the new insertion affects
                     # the min max cost of the solution)
                     # the fields of bestInsertion will be updated according to the new best insertion found
+                    if best_insertion.subjective_cost == best_insertion_for_customer.subjective_cost:
+                        insertions_with_same_subj_cost += 1 # + 1 because another insertion with the same cost with the
+                                                            # best_insertion was found
+                    else:
+                        insertions_with_same_subj_cost = 2 # if a new best_insertion is found initialize
+                                                           # insertions_with_same_subj_cost back to 2
                     best_insertion.customer = best_insertion_for_customer.customer
                     best_insertion.route = best_insertion_for_customer.route
                     best_insertion.cost = best_insertion_for_customer.cost
@@ -151,11 +174,11 @@ class SolverMinIns:
                     # the solution, then the difference between the new cost and the previous min max cost gets multiplied
                     # with siders_constant and the result gets added to the subjective cost.
                     # This is done in order to take into account if an insertion affects the min max cost of the solution
-                    if route.cost + trial_cost > self.sol.max_cost_of_route:
-                        subjective_cost_of_insertion += self.siders_constant *\
-                                                               (route.cost + trial_cost - self.sol.max_cost_of_route)
-
-
+                    dist_from_max_cost = route.cost + trial_cost - self.sol.max_cost_of_route
+                    if dist_from_max_cost > 0:
+                        subjective_cost_of_insertion += self.siders_constant * dist_from_max_cost
+                    else:
+                        subjective_cost_of_insertion += self.siders_constant2 * dist_from_max_cost
 
                     if  subjective_cost_of_insertion < best_insertion_for_customer.subjective_cost:
                          # if the best insertion of the checking customer is better than the best insertion at this moment
