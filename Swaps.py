a11e3ce347218fc55a6a8728eb514c9ffc4b4610
NikolasMoatsos <56679780+NikolasMoatsos@users.noreply.github.com>
2021-01-10 22:07:17 +0200

    Create Swaps.py

---
+++ Swaps.py
@@ -0,0 +1,232 @@
+from VRP_Model import *
+from VRPMinimumInsertions import *
+from SolutionDrawer import *
+
+class Solution:
+    def __init__(self): #sider
+        self.max_cost_of_route = 0.0
+        self.routes = []
+
+class SwapMove(object):
+    def __init__(self):
+        self.positionOfFirstRoute = None
+        self.positionOfSecondRoute = None
+        self.positionOfFirstNode = None
+        self.positionOfSecondNode = None
+        self.costChangeFirstRt = None
+        self.costChangeSecondRt = None
+        self.moveCost = None
+
+    def Initialize(self):
+        self.positionOfFirstRoute = None
+        self.positionOfSecondRoute = None
+        self.positionOfFirstNode = None
+        self.positionOfSecondNode = None
+        self.costChangeFirstRt = None
+        self.costChangeSecondRt = None
+        self.moveCost = 10 ** 9
+
+class Swaps:
+
+    def __init__(self, minIns):
+        # m is the model, siders_constant, siders_constant2 (the second is optional) are numbers that define how
+        # important is it for an insertion not to increase the min_max cost of the solution
+        self.allNodes = minIns.allNodes
+        self.customers = minIns.customers
+        self.depot = minIns.allNodes[0]
+        self.time_matrix = minIns.time_matrix
+        self.capacity = minIns.capacity
+        self.sol = minIns.sol
+        self.bestSolution = None
+
+    def solveSwaps(self): # with sort variable defines if the minimum_insertions_with_opened_routes will
+                                        # sort the self.customers
+        self.LocalSearch(1)
+        return self.sol
+
+    def FindRouteWithMaxCost(self): # mo
+        # function returning the route with the maximum cost and its index in the list of routes
+        for i in range(len(self.sol.routes)):
+            if self.sol.routes[i].cost == self.sol.max_cost_of_route:
+                return (i,self.sol.routes[i])
+       # return [(i,self.sol.routes[i]) if self.sol.routes[i].cost == self.sol.max_cost_of_route else None for i in range(len(self.sol.routes))]
+
+    def CalculateMaxCostOfRoute(self):  # sider
+        # returns the max cost of the routes in the current solution
+        # return max(
+        #     sum(
+        #         self.time_matrix[self.sol.routes[i].sequenceOfNodes[j].ID][self.sol.routes[i].sequenceOfNodes[j + 1].ID]
+        #         for j in range(len(self.sol.routes[i].sequenceOfNodes) - 1)
+        #     ) for i in range(len(self.sol.routes))
+        # )
+        return max(route.cost for route in self.sol.routes)  # if the routes costs are correct this will work, else
+        # try the commented piece of code
+
+    def LocalSearch(self, operator):
+        self.bestSolution = self.cloneSolution(self.sol)
+        terminationCondition = False
+        localSearchIterator = 0
+
+        sm = SwapMove()
+
+        while terminationCondition is False:
+
+            self.InitializeOperators(sm)
+            #SolDrawer.draw(localSearchIterator, self.sol, self.allNodes)
+
+            # Swaps
+            if operator == 1:
+                self.FindBestSwapMove(sm)
+                if sm.positionOfFirstRoute is not None:
+                    if sm.moveCost < 0:
+                        self.ApplySwapMove(sm)
+                    else:
+                        terminationCondition = True
+
+            self.TestSolution()
+
+            if (self.sol.max_cost_of_route < self.bestSolution.max_cost_of_route):
+                self.bestSolution = self.cloneSolution(self.sol)
+
+            localSearchIterator = localSearchIterator + 1
+
+        self.sol = self.bestSolution
+        print(localSearchIterator)
+
+    def FindBestSwapMove(self, sm):  # mo
+        unpack = self.FindRouteWithMaxCost()  # find the Route with the max cost and its index in the routes matrix
+        firstRouteIndex = unpack[0]  # unpack index
+        rt1 = unpack[1]  # unpack Route
+        for secondRouteIndex in range(firstRouteIndex, len(self.sol.routes)):  # for every route that has not been checked for the first root
+            rt2: Route = self.sol.routes[secondRouteIndex]  # the route from which a node will be swapped
+            for firstNodeIndex in range(1, len(rt1.sequenceOfNodes) - 1):  # for every node of the first route
+                startOfSecondNodeIndex = 1  # start index for the second route
+                if rt1 == rt2:  #if the routes are the same
+                    startOfSecondNodeIndex = firstNodeIndex + 1  # start one node forward to avoid checking the same ones
+                for secondNodeIndex in range(startOfSecondNodeIndex, len(rt2.sequenceOfNodes) - 1):  # for every node of the second route after the index we specified
+
+                    # nodes of the first route
+                    a1 = rt1.sequenceOfNodes[firstNodeIndex - 1]
+                    b1 = rt1.sequenceOfNodes[firstNodeIndex]
+                    c1 = rt1.sequenceOfNodes[firstNodeIndex + 1]
+
+                    # nodes of the second route
+                    a2 = rt2.sequenceOfNodes[secondNodeIndex - 1]
+                    b2 = rt2.sequenceOfNodes[secondNodeIndex]
+                    c2 = rt2.sequenceOfNodes[secondNodeIndex + 1]
+
+                    moveCost = None
+                    costChangeFirstRoute = None
+                    costChangeSecondRoute = None
+
+                    if rt1 == rt2:  # if the routes are same
+                        if firstNodeIndex == secondNodeIndex - 1:  # if the first node is behind the second node
+                            costRemoved = self.time_matrix[a1.ID][b1.ID] + self.time_matrix[b1.ID][b2.ID] + \
+                                          self.time_matrix[b2.ID][c2.ID]
+                            costAdded = self.time_matrix[a1.ID][b2.ID] + self.time_matrix[b2.ID][b1.ID] + \
+                                        self.time_matrix[b1.ID][c2.ID]
+                            moveCost = costAdded - costRemoved
+
+                        else:
+                            costRemoved1 = self.time_matrix[a1.ID][b1.ID] + self.time_matrix[b1.ID][c1.ID]
+                            costAdded1 = self.time_matrix[a1.ID][b2.ID] + self.time_matrix[b2.ID][c1.ID]
+                            costRemoved2 = self.time_matrix[a2.ID][b2.ID] + self.time_matrix[b2.ID][c2.ID]
+                            costAdded2 = self.time_matrix[a2.ID][b1.ID] + self.time_matrix[b1.ID][c2.ID]
+                            moveCost = costAdded1 + costAdded2 - (costRemoved1 + costRemoved2)
+                    else:
+                        if rt1.load - b1.demand + b2.demand > self.capacity:
+                            continue
+                        if rt2.load - b2.demand + b1.demand > self.capacity:
+                            continue
+
+                        costRemoved1 = self.time_matrix[a1.ID][b1.ID] + self.time_matrix[b1.ID][c1.ID]
+                        costAdded1 = self.time_matrix[a1.ID][b2.ID] + self.time_matrix[b2.ID][c1.ID]
+                        costRemoved2 = self.time_matrix[a2.ID][b2.ID] + self.time_matrix[b2.ID][c2.ID]
+                        costAdded2 = self.time_matrix[a2.ID][b1.ID] + self.time_matrix[b1.ID][c2.ID]
+
+                        costChangeFirstRoute = costAdded1 - costRemoved1
+                        costChangeSecondRoute = costAdded2 - costRemoved2
+                        if (rt1.cost + costChangeFirstRoute) > (rt2.cost + costChangeSecondRoute):
+                            moveCost = costChangeFirstRoute
+                        else:
+                            moveCost = rt2.cost + costChangeSecondRoute - self.sol.max_cost_of_route
+                    if moveCost < sm.moveCost and abs(moveCost) > 0.0001:
+                        self.StoreBestSwapMove(firstRouteIndex, secondRouteIndex, firstNodeIndex, secondNodeIndex,
+                                               moveCost, costChangeFirstRoute, costChangeSecondRoute, sm)
+
+
+    def ApplySwapMove(self, sm):
+        rt1 = self.sol.routes[sm.positionOfFirstRoute]
+        rt2 = self.sol.routes[sm.positionOfSecondRoute]
+        b1 = rt1.sequenceOfNodes[sm.positionOfFirstNode]
+        b2 = rt2.sequenceOfNodes[sm.positionOfSecondNode]
+        rt1.sequenceOfNodes[sm.positionOfFirstNode] = b2
+        rt2.sequenceOfNodes[sm.positionOfSecondNode] = b1
+
+        if (rt1 == rt2):
+            rt1.cost += sm.moveCost
+        else:
+            rt1.cost += sm.costChangeFirstRt
+            rt2.cost += sm.costChangeSecondRt
+            rt1.load = rt1.load - b1.demand + b2.demand
+            rt2.load = rt2.load + b1.demand - b2.demand
+
+        self.sol.max_cost_of_route = self.CalculateMaxCostOfRoute()  # find the new max cost after the relocation
+        self.TestSolution()
+
+
+    def StoreBestSwapMove(self, firstRouteIndex, secondRouteIndex, firstNodeIndex, secondNodeIndex, moveCost,
+                          costChangeFirstRoute, costChangeSecondRoute, sm):
+        sm.positionOfFirstRoute = firstRouteIndex
+        sm.positionOfSecondRoute = secondRouteIndex
+        sm.positionOfFirstNode = firstNodeIndex
+        sm.positionOfSecondNode = secondNodeIndex
+        sm.costChangeFirstRt = costChangeFirstRoute
+        sm.costChangeSecondRt = costChangeSecondRoute
+        sm.moveCost = moveCost
+
+    def cloneRoute(self, rt: Route):
+        cloned = Route(self.depot, self.capacity)
+        cloned.cost = rt.cost
+        cloned.load = rt.load
+        cloned.sequenceOfNodes = rt.sequenceOfNodes.copy()
+        return cloned
+
+    def cloneSolution(self, sol: Solution):
+        cloned = Solution()
+        for i in range(0, len(sol.routes)):
+            rt = sol.routes[i]
+            clonedRoute = self.cloneRoute(rt)
+            cloned.routes.append(clonedRoute)
+        cloned.max_cost_of_route = self.sol.max_cost_of_route
+        return cloned
+
+    def InitializeOperators(self, sm):
+        sm.Initialize()
+
+    def TestSolution(self):  # sider
+        if len(self.sol.routes) > 25:  # if the solution used more routes than the routes available
+            print("Routes' number problem.")
+        max_cost_of_route = 0
+        nodes_serviced = 0
+        for r in range(0, len(self.sol.routes)):
+            rt: Route = self.sol.routes[r]
+            nodes_serviced += len(rt.sequenceOfNodes) - 2 # -2 because we remove depot that exist twice in every route
+            rt_cost = 0
+            rt_load = 0
+            for n in range(0, len(rt.sequenceOfNodes) - 1):
+                A = rt.sequenceOfNodes[n]
+                B = rt.sequenceOfNodes[n + 1]
+                rt_cost += self.time_matrix[A.ID][B.ID]
+                rt_load += A.demand
+            if abs(rt_cost - rt.cost) > 0.0001:
+                print('Route Cost problem')
+            if rt_load != rt.load:
+                print('Route Load problem')
+            if rt_cost > max_cost_of_route:
+                max_cost_of_route = rt_cost
+        if abs(max_cost_of_route - self.sol.max_cost_of_route) > 0.0001:
+            print('Solution Cost problem, solution cost: ' + str(self.sol.max_cost_of_route) +
+                  ' calculated cost: ' + str(self.CalculateMaxCostOfRoute()))
+        if nodes_serviced != len(self.customers):
+            print('Number of serviced nodes problem')
